f57776c83ed0417ecd089b23823d805d7771208d
Lunagp <lunagp03@gmail.com>
2022-08-25 09:15:49 -0500

    login mapeado

--- src/main/java/co/com/instagram/userinterfaces/InstagramHome.java
+++ src/main/java/co/com/instagram/userinterfaces/InstagramHome.java
@@ -1,4 +1,8 @@
 package co.com.instagram.userinterfaces;
 
 public class InstagramHome {
+
+    public static final Target USERNAME = Target.the("campo de usuario").located(By.xpath("//*[@name=\"username\"]"));
+    public static final Target PASSWORD = Target.the("camo de password").located(By.xpath("//*[@name=\"password\"]"));
+    public static final Target BTN_GETIN = Target.the("boton de entrada").located(By.xpath"//*[@name=\"password\"]")
 }
